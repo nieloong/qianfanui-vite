dab86e4da3a3bc8160418bf6b82f85383576ba10
LoongKingBoss <641728086@qq.com>
2022-04-12 09:01:47 +0800

    20220412 555222

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />20220408 -- 来米撒
+  <img alt="Vue logo" src="./assets/logo.png" />20220408 -+- 来米撒
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
